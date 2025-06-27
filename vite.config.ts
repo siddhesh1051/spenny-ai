abd75f7ed61d3d893d4e0d9775779de5d1153c19
siddhesh-turbostart <siddhesh.p@tsquaredc.com>
2025-06-28 00:28:43 +0530

    added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
+      injectRegister: "auto",
       manifest: {
         name: "Spenny AI",
         short_name: "Spenny",
